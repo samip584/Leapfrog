b8a70d99e0140d14631627b2b8aeecd7e7213040
unknown <samip0884@gmail.com>
2020-07-12 17:42:35 +0545

    Add mobile

--- Final Project/Guitar Tuner/js/initializer.js
+++ Final Project/Guitar Tuner/js/initializer.js
@@ -50,7 +50,8 @@ function getUserMedia(dictionary, callback) {
       navigator.getUserMedia = 
         navigator.getUserMedia ||
         navigator.webkitGetUserMedia ||
-        navigator.mozGetUserMedia;
+        navigator.mozGetUserMedia ||
+        navigator.mediaDevices.getUserMedia({ audio: true, video: false });
       navigator.getUserMedia(dictionary, callback, error);
   } catch (e) {
       alert('getUserMedia threw exception :' + e);
