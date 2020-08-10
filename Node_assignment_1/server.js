99a31fa75ee306ef32c656cc53416e9c6ddd8da2
unknown <samip0884@gmail.com>
2020-08-10 21:47:27 +0545

    Add: Node assignment 1

---
+++ Node_assignment_1/server.js
@@ -0,0 +1,53 @@
+const http = require('http');
+
+const fileOperation = require('./file');
+
+const server = http.createServer(function(req, res){
+  let urlComponents =  req.url.split('/');
+  switch(urlComponents[1]){
+    case '':
+      res.end('Welcome');
+    case 'write':
+      fileOperation.write(urlComponents[2]+'.txt', urlComponents[3])
+      .then(function(data){
+        res.end('success');
+      })
+      .catch(function(err){
+        res.end('failure >>', err);
+      })
+      break
+    case 'read':
+      fileOperation.read(urlComponents[2]+'.txt')
+      .then(function(data){
+        res.end(data);
+      })
+      .catch(function(err){
+        res.end('failure >>', err);
+      })
+      break
+    case 'rename':
+      fileOperation.rename(urlComponents[2]+'.txt',urlComponents[3]+'.txt')
+      .then(function(data){
+        res.end(urlComponents[2]+'.txt was named '+ urlComponents[3]+'.txt');
+      })
+      .catch(function(err){
+        res.end('failure >>', err);
+      })
+      break
+    case 'delete':
+      fileOperation.del(urlComponents[2]+'.txt')
+      .then(function(data){
+        res.end(urlComponents[2]+'.txt was deleted');
+      })
+      .catch(function(err){
+        res.end('failure >>', err);
+      })
+      break
+  }
+})
+
+server.listen(8000, function() {
+  console.log('server is up and running at port 8000')
+  console.log('press CTRL +C to exit')
+})
+
